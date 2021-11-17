8c81d99c13a7a4e83f56b97fd7c5192be2b72c3c
vwiklund <v.wiklund@hotmail.com>
2021-11-17 23:38:11 +0100

    s

--- Hw2/main2.py
+++ Hw2/main2.py
@@ -7,7 +7,7 @@ from P4 import P4
 from P8 import P8
 from P9 import P9
 from P10 import P10
-from P11 import P11
+from P11 import P11,P11test#
 
 #P1(L=5,r=2,nr=0,N=10,part = "a",step=1)
 #P2(N=15,L=5,r=2,nr=0)
@@ -30,14 +30,14 @@ from P11 import P11
 #P9(L=1000,N=100,v=3,dt=1,noice=0.4,steps=1000,r=20,h=-2)
 #P9(L=1000,N=100,v=3,dt=1,noice=0.4,steps=1000,r=20,h=-5)
 
-<<<<<<< Updated upstream
-P10(L=100,N=100,v=1,dt=1,noice=0.1,steps=3000,r=1,k=4)
-=======
+
+#P10(L=100,N=100,v=1,dt=1,noice=0.1,steps=3000,r=1,k=4)
+
 #P10(L=100,N=100,v=1,dt=1,noice=0.1,steps=3000,r=1,k=2)
 
 #P11test(L=10,N=25,v=1,dt=1,noice=0.1,steps=3000,r=5, alpha=np.pi/2)
 
 #P11(L=100,N=100,v=1,dt=1,noice=0.1,steps=3000,r=1,alpha = np.pi/4)
->>>>>>> Stashed changes
+
 
 
