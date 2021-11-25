92bd75b32b5aa8cd81ce61c0b9c30f0fd035c4df
WiklundViktor <viktor.wiklund@weknowit.nu>
2021-11-25 15:50:24 +0100

    g

--- Hw3/back.py
+++ Hw3/back.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 #print(np.load("R(beta,gamma=0,01).npy"))
 #print(np.load("R(beta,gamma=0,02).npy"))
 #avg = 2
@@ -37,6 +40,8 @@ beta = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
 gamma = [0.01,0.02]
 R1 = [45,330,650,760,840,850,860,840,850,860]
 R2 = [45,50,110,200,390,500,600,630,690,700]
+
+
 #print(len(R2),len(beta))
 #plt.plot(beta,R1,"o")
 #plt.plot(beta,R2,"o")
@@ -56,38 +61,64 @@ bg = np.concatenate((bg, np.linspace(60,100,5)))
 #0.1,.01 & 0.2,0.02
 #print(beta_gamma0)
 #print(beta_gamma1)
-print(X)
-print(bg)
-i = 0
+
+i = 6
 j = 0
 print(X[i]/gamma[j])
-RR=np.zeros([10,10])
+RR=np.zeros([len(bg),len(X)])
+R1 = [45,330,650,760,840,850,860,840,850,860]
+R2 = [45,50,110,200,390,500,600,630,690,700]
+# = 5
+RR[0,0] = 45
 
-RR[0,0] = 0
-RR[0,1] = RRR[1][0]
+RR[1,1] = 50
+RR[1,0] = 45
+# = 15
+RR[2,2] = 110
+#20
+RR[3,3] = 200
+RR[3,1] = 330
+#25
+RR[4,4] = 390
+#30
+RR[5,5] = 650
+RR[5,5] = 500
+#35
+RR[6,6] = 600
+#40
+RR[7,7] = 840
+RR[7,3] = 760
 
-RR[1,0] = RRR[0][0]
-RR[1,1] = RRR[1][1]
+#45
+RR[8,8] = 690
 
-RR[2,0] = 0
-RR[2,1] = 0
+#50
+RR[9,9] = 700
+RR[9,4] = 840
 
-RR[3,0] = 0
-RR[3,1] = 0
+#60
+RR[10,5] = 850
 
-RR[4,0] = 0
-RR[4,1] = 0
+#70
+RR[11,6] = 860
+#80
+RR[12,7] = 840
 
-RR[5,0] = 0
-RR[5,1] = 0
+RR[13,8] =850
 
-RR[6,0] = 0
-RR[6,1] = 0
+RR[12,9] =860
 
-RR[7,0] = 0
-RR[7,1] = 0
 
-print(RR)
 
-plt.contourf(X,bg,RR)
+
+print(X)
+print(bg)
+#print(RR)
+
+plt.contour(X,bg,RR)
+#plt.plot(R,)
+plt.xlabel("beta")
+plt.ylabel("beta/gamma")
+plt.title("contourplot of #recovery as a function of beta and beta/gamma")
+plt.savefig("contour")
 plt.show()
