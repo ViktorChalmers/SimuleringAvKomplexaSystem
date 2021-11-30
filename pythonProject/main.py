886dd3827b623323adb12b155b06283a11070901
vwiklund <v.wiklund@hotmail.com>
2021-11-30 15:27:45 +0100

    d

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 
-def getStats(N,n,m,reward):#return stats for player with strategy n
+def getScore(N,n,m,reward):#return stats for player with strategy n
     T = reward[0]
     R = reward[1]
     P = reward[2]
@@ -11,7 +12,6 @@ def getStats(N,n,m,reward):#return stats for player with strategy n
     stats = 0
     for i in range(N):
         if i <= n - 1 and i <= m - 1:
-
             stats += R
         elif n < m:
             stats += T
@@ -87,40 +87,100 @@ def getVonNeumannNeighbors(expandedState,index):
     #print(list)
     return [self, list]
 
-def playGame(state,N,reward):
+def getStats(state,N,reward):
     expandedState = expandPeriodic(state)
-    print(state)
+    stats = np.zeros([len(state), len(state)])
     for i in range(len(state)):
         for j in range(len(state)):
             index = np.array([i,j])
             [selfStrategy, neigborStrategy] = getVonNeumannNeighbors(expandedState,index)
             #print(neigborStrategy)
-            stats = np.zeros(4)
+            score = 0
             for k in range(4):
-                stats[k] = getStats(N, selfStrategy, neigborStrategy[k], reward)
-            print(stats)
-            minNeigbourIndex = np.argmax(stats)
-            if getStats(N, minNeigbourIndex, selfStrategy,  reward) < stats[minNeigbourIndex]:
-                state[index[0],index[1]] = neigborStrategy[minNeigbourIndex]
-    print(state)
+                score = score + getScore(N, selfStrategy, neigborStrategy[k], reward)
+            stats[i,j] = score
+    return stats
+
+def playGame(state,N,reward):
+    stats = getStats(state,N,reward)
+
+    expandedStats = expandPeriodic(stats)
+    expandedState = expandPeriodic(state)
+    for i in range(len(state)):
+        for j in range(len(state)):
+            index = np.array([i, j])
+            [selfScore, neigborScore] = getVonNeumannNeighbors(expandedStats,index)
+            np.argmin(neigborScore)
+            minIndex = np.where(neigborScore == neigborScore.min())[0]
+            minIndex = minIndex[np.random.randint(len(minIndex))]
+
+
+
+            if neigborScore[minIndex] < selfScore:
+
+                if minIndex == 0:
+                    state[i, j] = expandedState[i-1+1, j+1]
+
+                if minIndex == 1:
+                    state[i, j] = expandedState[i+1+1, j+1]
+
+                if minIndex == 2:
+                    state[i, j] = expandedState[i+1, j-1+1]
+
+                if minIndex == 3:
+                    state[i, j] = expandedState[i+1, j+1+1]
+
     return state
+def P2(part="a", L = 10, N=9, reward=[0,0.5,1,1.5],plot = False):
 
-
-def P2(part="a", L = 10, N=9, reward=[0,0.5,1,1.5]):
     if part == "a":
-        state = np.ones([L, L])*N
-        #state = np.random.randint(10,size=[L,L])
+        initState = np.ones([L, L])*N
         mid = int(L/2)
-        state[mid,mid] = 0
+        initState[mid,mid] = 0
+        stateList = [initState]
+        state = initState
 
-        for i in range(2):
-            state = playGame(state,N,reward)
+        fps = 10
+        nSeconds = 3
+        for i in range(fps*nSeconds):
+            state = playGame(state, N, reward)
+            stateList.append(np.copy(state))
+            #plt.imshow(stateList[i])
+            #plt.show()
+    if plot:
+        fig = plt.figure()
+
+        #stateList = [state]
+        #initState = [state]
+        im = plt.imshow(initState, interpolation='none', aspect='auto', vmin=0, vmax=1)
+        fps = 5
+        nSeconds = 5
+        def animate_func(i):
+            if i % fps == 0:
+                print('.', end='')
+            im.set_array(stateList[i])
+            plt.title(f"R = {R}, one centered defected: Generation {i}")
+            return [im]
+
+        anim = animation.FuncAnimation(
+            fig,
+            animate_func,
+            frames=nSeconds * fps,
+            interval=1000 / fps,  # in ms
+        )
+        saveName = f'2a;R={R}'.replace(".",",")+".gif"
+        print("saving:"+saveName)
+        #plt.show()
+        anim.save(saveName, writer='imagemagick')
 
 
 if __name__ == '__main__':
     #P1(part="a",N=10,reward=[0,0.5,1,1.5],m=10)
     #P1(part="b",N=10,reward=[0,.01,1,10])
-    R = 0.9
-    P2(part="a", L=10, N=10, reward=[0,R,1,1.5])
-    #print(getStats(N=10, n=10, m=10, reward=[0, R, 1, 1.5]))
+    #R = 0.88
+    #P2(part="a", L=30, N=7, reward=[0,R,1,1.5], plot = True)
+    Rt = 0.88
+    for r in range(13):
+        R = Rt+r/100
+        P2(part="a", L=30, N=7, reward=[0, R, 1, 1.5], plot=True)
 
